20e74e7f11a0fcb5ef2c3291c8bc181c23b4feba
Mosa6 <tikamongi@gmail.com>
2022-04-05 08:00:14 +0000

    [Finish] 2-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_075533) do
+ActiveRecord::Schema.define(version: 2022_04_05_075817) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
 end
